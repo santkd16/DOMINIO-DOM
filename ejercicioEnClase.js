816079300cae2b265330777060d0fd9b01d3602b
DAVID SANTIAGO PIEDRAHITA CABEZA <145588434+santkd16@users.noreply.github.com>
2024-04-05 23:12:57 -0500

    Update ejercicioEnClase.js

--- ejercicioEnClase.js
+++ ejercicioEnClase.js
@@ -1,13 +1,9 @@
-console.log("-----------Elementos----------------")
 
-/*5.Punto*/
 let h=document.querySelector('h1') 
 console.log(h)
 
 let parrafo=document.querySelector('.pp') 
 console.log(parrafo)
-
-/*6.Punto*/
 
 console.log("Texto de la etiqueta h1:")
 console.log(h.textContent)
